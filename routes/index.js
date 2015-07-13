3a004978dc36e4840b051d1bf2a259d1135e6cd5
Luis Mendoza <lmendoza.mg@gmail.com>
2015-07-13 00:04:44 -0500

    Informacion de autor

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Quiz' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
