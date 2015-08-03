7585c86902ab61f21ad564fb0bab8ac4a38e8203
Luis Mendoza <lmendoza.mg@gmail.com>
2015-08-03 18:42:09 -0500

    Agregada Base de Datos

---
+++ models/models.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(null, null, null,
+	{dialect: "sqlite", storage: "quiz.sqlite"});
+
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz;
+
+sequelize.sync().then(function () {
+	Quiz.count().then(function (count) {
+		if(count === 0){
+			Quiz.create({
+				pregunta: 'Capital de Colombia',
+				respuesta: 'Bogota'
+			})
+			.then(function () {
+				console.log('Base de datos inicializada')
+			});
+		}
+	});
+});
