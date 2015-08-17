886298c6a25a93585e9c4b3a8adea24178515c02
Luis Antonio <lmendoza.mg@gmail.com>
2015-08-17 12:45:46 -0500

    crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -25,3 +25,19 @@ exports.index = function(req, res){
 		res.render('quizes/index.ejs', {quizes: quizes});
 	});
 };
+
+exports.new = function(req, res){
+	var quiz = models.Quiz.build(
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+exports.create = function(req, res){
+	var quiz = models.Quiz.build(
+		req.body.quiz
+	);
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function () {
+		res.redirect("/quizes");
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,7 @@ router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+
 module.exports = router;
