e66cc794c2d1624cd5516f7558899c6fb08415a2
Luis Antonio <lmendoza.mg@gmail.com>
2015-08-17 20:18:22 -0500

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,29 @@
 var models = require('../models/models.js');
 
+exports.load = function(req, res, next, quizId){
+	models.Quiz.findById(quizId).then(function (quiz) {
+		if(quiz){
+			req.quiz = quiz;
+			next();
+		}else{
+			next(new Error('No existe quizId='+quizId));
+		}
+	}).catch(function (error) {
+		next(error);
+	});
+};
+
 // GET /quizes/question
 exports.show = function(req, res){
 	models.Quiz.findById(req.params.quizId).then(function (quiz) {
-		res.render('quizes/show.ejs', {quiz: quiz, errors: []});
+		res.render('quizes/show.ejs', {quiz: req.quiz, errors: []});
 	});
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
 	models.Quiz.findById(req.params.quizId).then(function (quiz) {
-		if(req.query.respuesta === quiz.respuesta)
+		if(req.query.respuesta === req.quiz.respuesta)
 			res.render('quizes/answer', {respuesta: 'Correcto', errors: []});
 		else
 			res.render('quizes/answer', {respuesta: 'Incorrecto', errors: []});
@@ -47,4 +60,34 @@ exports.create = function(req, res){
 			});
 		}
 	})
+};
+
+exports.edit = function(req, res){
+	var quiz = req.quiz;
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	console.log("asdasd");
+
+	req.quiz
+	.validate()
+	.then(
+		function (err) {
+			if(err){
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			}else{
+				req.quiz
+				.save({fields: ["pregunta", "respuesta"]})
+				.then(function () {
+					res.redirect('/quizes');
+				});
+			}
+		}
+	);
+
+	
 };
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
 
+router.param('quizId', quizController.load); // autoload: quizId
+
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Quiz', errors: []});
 });
@@ -18,5 +20,9 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
+
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
+
 
 module.exports = router;
