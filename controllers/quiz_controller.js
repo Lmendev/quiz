ede7a3e6f39c68a4eb9b4dfdad858d11584483e0
Luis Mendoza <lmendoza.mg@gmail.com>
2015-08-19 14:02:29 -0500

    Crear comentario

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,7 +1,10 @@
 var models = require('../models/models.js');
 
 exports.load = function(req, res, next, quizId){
-	models.Quiz.findById(quizId).then(function (quiz) {
+	models.Quiz.find({
+		where: {id: Number(quizId)},
+		include: [{model: models.Comment }]
+	}).then(function (quiz) {
 		if(quiz){
 			req.quiz = quiz;
 			next();
--- models/models.js
+++ models/models.js
@@ -6,8 +6,13 @@ var sequelize = new Sequelize(null, null, null,
 	{dialect: "sqlite", storage: "quiz.sqlite"});
 
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz;
+exports.Comment = Comment;
 
 sequelize.sync().then(function () {
 	Quiz.count().then(function (count) {
